8549c20541352279da6a97b320de90165de75e47
dan sinclair <dan.sinclair@cds-snc.ca>
2019-07-26 12:03:51 -0400

    Minor controller changes.
    This CL limits the index page to 25 random entries along with limiting
    the search results to 25 records. The search detection of the searchable
    type has also been fixed.

--- app/controllers/api/v1/procurements_controller.rb
+++ app/controllers/api/v1/procurements_controller.rb
@@ -2,9 +2,11 @@ module Api::V1
 
 class ProcurementsController < ApplicationController
   def index
-    @procurements = Procurement.all.includes(
+    # TODO(dan.sinclair): This finds a random 25 elements, probably not what
+    #  is desired in the future.
+    @procurements = Procurement.order("RANDOM()").includes(
         :trade_agreements, :items, :recurrences, contact: [
-          :languages, :procuring_entity])
+          :languages, :procuring_entity]).first(25)
     render jbuilder: @procurements
   end
 
@@ -15,12 +17,13 @@ class ProcurementsController < ApplicationController
 
   def search
     @procurements = []
-    results = PgSearch.multisearch(params[:q])
+    # TODO(dan.sinclair): Artificially limited to 25 responses.
+    results = PgSearch.multisearch(params[:q]).first(25)
     results.each do |res|
-      if res.searchable.is_a?(Procurement)
+      if res.searchable_type == "Procurement"
         @procurements << res.searchable
-      elsif res.searchable.is_a?(ProcurementItem) ||
-          res.searchable.is_a?(ProcurementTradeAgreement)
+      elsif res.searchable_type == "ProcurementItem" ||
+          res.searchable_type == "ProcurementTradeAgreement"
         @procurements << res.searchable.procurement
       else
         @procurements << res.searchable.procurements
